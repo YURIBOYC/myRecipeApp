c24f4b025f55ad91dcea57a0ae2ad17c32fbe760
ABISHEK <abideepu19.vrs@gmail.com>
2024-12-22 18:28:12 +0530

    Header Design
    Designing the Header Section

---
+++ myRecipeApp/src/Header.jsx
@@ -0,0 +1,10 @@
+export default function Header() {
+  return (
+    <header className="recipe-header">
+      <nav className="recipe-nav">
+        <img src="/src/assets/chef.png" className="chef-logo" />
+        <p className="chef-heading">Abishek's Recipe</p>
+      </nav>
+    </header>
+  );
+}
