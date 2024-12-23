82b490af024e5cb6faf258c80cf70f67da7bd6a4
ABISHEK <abideepu19.vrs@gmail.com>
2024-12-23 18:03:36 +0530

    Ingredients Div

--- myRecipeApp/src/Body.jsx
+++ myRecipeApp/src/Body.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 export default function Main() {
-  const [ingredients, setIngredients] = React.useState(["Potato"]);
+  const [ingredients, setIngredients] = React.useState([]);
 
   const ingEl = ingredients.map((item) => {
     return <li key={item}>{item}</li>;
@@ -24,7 +24,21 @@ export default function Main() {
         />
         <button>Add Ingredient</button>
       </form>
-      <ul>{ingEl}</ul>
+      {ingredients.length > 0 ? (
+        <section>
+          <h2>Ingredients on hand:</h2>
+          <ul className="ingredients-list" aria-live="polite">
+            {ingEl}
+          </ul>
+          <div className="get-recipe-container">
+            <div>
+              <h3>Ready for a recipe?</h3>
+              <p>Generate a recipe from your list of ingredients.</p>
+            </div>
+            <button>Get a recipe</button>
+          </div>
+        </section>
+      ) : null}
     </main>
   );
 }
