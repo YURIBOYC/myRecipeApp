0b003ea999374c2a9d5d1026eb63458303432b63
ABISHEK <abideepu19.vrs@gmail.com>
2024-12-23 11:32:42 +0530

    Array State

--- myRecipeApp/src/Body.jsx
+++ myRecipeApp/src/Body.jsx
@@ -1,14 +1,32 @@
-export default function Body() {
+import React from "react";
+export default function Main() {
+  const [ingredients, setIngredients] = React.useState(["Potato"]);
+
+  const ingEl = ingredients.map((item) => {
+    return <li key={item}>{item}</li>;
+  });
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget);
+    const newIngredient = formData.get("ingredient");
+    setIngredients((prevIngredients) => {
+      return [...prevIngredients, newIngredient];
+    });
+  }
+
   return (
     <main className="main-div">
-      <form className="ingredient-form">
+      <form className="ingredient-form" onSubmit={handleSubmit}>
         <input
           type="text"
           aria-label="Ingredient Box"
           placeholder="e.g. Tomato"
+          name="ingredient"
         />
         <button>Add Ingredient</button>
       </form>
+      <ul>{ingEl}</ul>
     </main>
   );
 }
