327364a535a7f2932bb05c51320bd538308292af
ABISHEK <abideepu19.vrs@gmail.com>
2024-12-25 22:52:08 +0530

    Correcting SVG and Chef Png

--- myRecipeApp/src/Header.jsx
+++ myRecipeApp/src/Header.jsx
@@ -1,8 +1,10 @@
+import chef from "/public/images/assets/chef.png";
+
 export default function Header() {
   return (
     <header className="recipe-header">
       <nav className="recipe-nav">
-        <img src="/src/assets/chef.png" className="chef-logo" />
+        <img src={chef} className="chef-logo" />
         <p className="chef-heading">Abishek's Recipe</p>
       </nav>
     </header>
